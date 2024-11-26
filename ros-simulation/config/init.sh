282bdab4d87e2dce9ea2666d0cb363e2deedbca5
vinayakaraju46 <vinayakaraju46@gmail.com>
2024-11-26 06:31:07 +0100

    [UPDATE] Compose file to manage network

--- ros-simulation/config/init.sh
+++ ros-simulation/config/init.sh
@@ -6,6 +6,13 @@ export NOVNC_PORT="6080"
 export DEBIAN_FRONTEND=noninteractive TZ="Etc/UTC" &&
 
 apt -y update && 
+
+# Set up the VNC password
+mkdir -p ~/.vnc
+echo "$VNC_PASSWD" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+
+
 vncserver 
 ttyd -p $TTYD_PORT bash &
 /usr/share/novnc/utils/launch.sh --vnc 127.0.0.1:5901 --listen $NOVNC_PORT &
