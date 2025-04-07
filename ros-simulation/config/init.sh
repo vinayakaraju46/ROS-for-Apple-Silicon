0b083f134503eb1405f3eaf4411219e9ab4f3d12
vinayakaraju46 <vinayakaraju46@gmail.com>
2025-04-07 13:25:57 +0200

    [UPDATE] ros2-humble support

--- ros-simulation/config/init.sh
+++ ros-simulation/config/init.sh
@@ -2,7 +2,7 @@
 export VNC_PASSWD="121212"
 export TTYD_PORT="5000"
 export NOVNC_PORT="6080"
-
+export HOME="/root"
 export DEBIAN_FRONTEND=noninteractive TZ="Etc/UTC" &&
 
 apt -y update && 
@@ -15,9 +15,8 @@ echo $VNC_PASSWD | vncpasswd -f > /root/.vnc/passwd
 vncserver -kill :1
 vncserver -PasswordFile /root/.vnc/passwd :1 &
 
-ttyd -p $TTYD_PORT bash &
+echo 'PS1="\[\033[1;31m\u@\h\[\033[0;31m\[\033[0;37m\] [\W]\[\033[00m # "' >> /root/.bashrc
+ttyd -p $TTYD_PORT -t 'theme={"foreground":"#fff","background":"#111", "cursor":"#943124"}' bash &
 /usr/share/novnc/utils/launch.sh --vnc 127.0.0.1:5901 --listen $NOVNC_PORT &
 
-chmod +x /config/restartvncserver.sh 
-chmod +x /config/startGazebo.sh 
-chmod +x /config/startTurtlesim.sh
+chmod +x /config/restartvncserver.sh
